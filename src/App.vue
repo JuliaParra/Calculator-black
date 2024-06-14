3a8f089327c8d1f6aa91f355f22cfd88f86ed80a
Antonella Enriquez Ledesma <your-email-address~>
2024-06-14 09:28:42 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import header from 'header'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+   
 
       <nav>
         <RouterLink to="/">Calculator</RouterLink>
         <RouterLink to="/div">Divisas</RouterLink>
         <RouterLink to="/tiemp">Tiempo</RouterLink>
       </nav>
-    </div>
+   
   </header>
 
   <RouterView />
---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+      <div>
+        <b-nav tabs fill>
+          <b-nav-item active>Calculator</b-nav-item>
+          <b-nav-item>Divisas</b-nav-item>
+          <b-nav-item>Tiempo</b-nav-item>
+        </b-nav>
+      </div>
+    </header>
+
+</template>
